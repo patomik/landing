e330d9b7bb67199e298b6ddd031d71f3ebfa7196
pk <pk@theinvaders.pr>
2020-07-08 21:42:54 +0300

    validate form fields

--- src/app/components/user-form-one/user-form-one.component.ts
+++ src/app/components/user-form-one/user-form-one.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormSwitcherService } from 'src/app/shared/services/form.service';
 import { UserService } from 'src/app/shared/services/user.service';
 import { UserData } from 'src/app/shared/interfaces/userData';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form-one',
@@ -11,16 +12,30 @@ import { UserData } from 'src/app/shared/interfaces/userData';
 export class UserFormOneComponent implements OnInit {
   isAgreementAccept = false;
   userData = {} as UserData;
+  registerForm: FormGroup;
+    submitted = false;
   mobNumberPattern = '^((\\+91-?)|0)?[0-9]{10}$';
-  constructor(private formSwitcherService: FormSwitcherService, private userService: UserService) {}
+  constructor(private formSwitcherService: FormSwitcherService, private userService: UserService, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.userService.userData.subscribe(ud => this.userData = ud );
+    this.registerForm = this.formBuilder.group({
+      phone: ['', Validators.required ],
+      email: ['', [Validators.required, Validators.email]],
+      agreementAccept: [false, Validators.requiredTrue]
+  });
   }
   switchNextForm(numForm: number) {
     this.formSwitcherService.changeNumberForm(numForm);
   }
   setUserData(key: string, value: any) {
     this.userService.setNewUserData(key, value);
+  }
+
+  get f() { return this.registerForm.controls; }
+  onSubmit( userForm) {
+    console.log('userForm :>> ', userForm);
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.userData, null, 4));
+    this.switchNextForm(2);
   }
 }
--- src/app/shared/directives/phone.directives.ts
+++ src/app/shared/directives/phone.directives.ts
@@ -20,22 +20,22 @@ export class PhoneMaskDirective {
 
 
   onInputChange(event, backspace) {
-    let newVal = event.replace(/\D/g, '');
-    if (backspace && newVal.length <= 6) {
-      newVal = newVal.substring(0, newVal.length - 1);
+      let newVal = (event) ?  event.replace(/\D/g, '') : '';
+      if (backspace && newVal.length <= 6) {
+        newVal = newVal.substring(0, newVal.length - 1);
+      }
+      if (newVal.length === 0) {
+        newVal = '';
+      } else if (newVal.length <= 3) {
+        newVal = newVal.replace(/^(\d{0,3})/, '($1)');
+      } else if (newVal.length <= 6) {
+        newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1) $2');
+      } else if (newVal.length <= 10) {
+        newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+      } else {
+        newVal = newVal.substring(0, 10);
+        newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+      }
+      return this.ngControl.valueAccessor.writeValue(newVal);
     }
-    if (newVal.length === 0) {
-      newVal = '';
-    } else if (newVal.length <= 3) {
-      newVal = newVal.replace(/^(\d{0,3})/, '($1)');
-    } else if (newVal.length <= 6) {
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1) $2');
-    } else if (newVal.length <= 10) {
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
-    } else {
-      newVal = newVal.substring(0, 10);
-      newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
-    }
-    this.ngControl.valueAccessor.writeValue(newVal);
-  }
 }
